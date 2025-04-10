109afa995855a88b84e729f31c5ad7937e9520fb
perkimbjm <108646533+perkimbjm@users.noreply.github.com>
2025-04-10 21:48:52 +0800

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { useQueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 
@@ -23,7 +23,7 @@ import WasteManagementSchedule from "./pages/WasteManagementSchedule";
 import ScheduleDetail from "./pages/ScheduleDetail";
 import { SidebarProvider } from "./components/ui/sidebar";
 
-const queryClient = useQueryClient();
+const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
